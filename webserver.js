cb2aa09a51db2a097ec2c78d5c99c61f4f58ad6b
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-01-14 01:30:18 -0600

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -10,18 +10,6 @@ server.all(`/`, (req, res) => {
   res.send(`Air [SERVER]\n running ` + bigstring);
 });
 
-async function pingUrl() {
-  try {
-    const response = await axios.get("https://dccmqn-3000.csb.app");
-    console.log(`Ping successful! Status code: ${response.status}`);
-  } catch (error) {
-    console.error(
-      `Error pinging URL: ${"https://dccmqn-3000.csb.app"}`,
-      error.message,
-    );
-  }
-}
-
 function runserver() {
   server
     .listen(3000, () => {
@@ -30,7 +18,6 @@ function runserver() {
     .on("error", (error) => {
       console.log(`[SERVER ERROR] error starting server `, error);
     });
-  setInterval(pingUrl, 60000);
 }
 
 module.exports = runserver;
