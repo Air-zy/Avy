15aa3d586f962bcf3a27ef588d49ea164f4b5819
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-01-19 15:55:15 -0600

    Update open_ai.js

--- modules/open_ai.js
+++ modules/open_ai.js
@@ -1,14 +1,32 @@
 const fetch = require('node-fetch');
 const apiKey = process.env['aikey'];
 
-async function generate(msgs) {
+async function generate(msgs, aimodel) {
   const body = {
     "messages": msgs,
-    "model": "gpt-3.5-turbo-1106",
+    "model": aimodel,
     "temperature": 0.8,
     "presence_penalty": 0.5,
-    "frequency_penalty": 0.8,
+    "frequency_penalty": 1,
     "top_p": 0.8,
+    "logit_bias": {
+        "68697": -100, // sorry
+        "19701": -100, // Sorry
+        "3923": -50, // What
+        "19182": -50, // Hey
+        "8823": -50, // help
+        "34360": -100, // cannot
+        "58369": -50, // digital
+        "26752": -50, // virtual
+        "31": -100, // @
+        "93": 5, // ~
+        "596": -10, // 's
+        "19643": 5, // sure
+        "9336": 5, // well
+        "2079": -50, // request
+        "9": 5, // *
+        "0": -30 // !
+    }
   }
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: 'POST',
