23792f2f480dc782b693c7ebc30a0819a8db7744
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-01-23 00:46:13 -0600

    Update open_ai.js

--- modules/open_ai.js
+++ modules/open_ai.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const apiKey = process.env['aikey'];
+const fs = require('fs');
 
+let tokenusage = 0
 async function generate(msgs, aimodel) {
   const body = {
     "messages": msgs,
@@ -14,16 +16,20 @@ async function generate(msgs, aimodel) {
         "19701": -100, // Sorry
         "3923": -50, // What
         "19182": -50, // Hey
-        "8823": -50, // help
+        "8823": -100, // help
+        "52066": -100, // assist
         "34360": -100, // cannot
         "58369": -50, // digital
         "26752": -50, // virtual
+        "82630": -50, // friendly
         "31": -100, // @
         "93": 5, // ~
+        "2201": 10, // no
         "596": -10, // 's
         "19643": 5, // sure
         "9336": 5, // well
         "2079": -50, // request
+        "29589": -50, // :)
         "9": 5, // *
         "0": -30 // !
     }
@@ -37,9 +43,26 @@ async function generate(msgs, aimodel) {
     "body": JSON.stringify(body),
   });
   const nresp = await response.json()
-  return nresp["choices"][0]["message"]["content"]
+  if ('usage' in nresp) {
+    const tokensUsed = nresp.usage.total_tokens;
+    tokenusage += tokensUsed
+    
+    let update = JSON.parse(fs.readFileSync('json_storage/info.json', 'utf-8'));
+    update.token_usage = tokenusage;
+    fs.writeFileSync('json_storage/info.json', JSON.stringify(update, null, 2));
+  }
+  if (nresp["choices"]) {
+    return nresp["choices"][0]["message"]["content"]
+  } else {
+    throw nresp;
+  }
+}
+
+function getusage(){
+  return tokenusage
 }
 
 module.exports = {
   generate,
+  getusage
 };
