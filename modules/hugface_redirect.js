fe4f43b8cc735df813c6e16a6a4972ac2db7823a
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-01-23 00:46:06 -0600

    Update hugface_redirect.js

--- modules/hugface_redirect.js
+++ modules/hugface_redirect.js
@@ -13,18 +13,20 @@ async function readStreamBody(response) {
     try {
       return JSON.parse(jsonString);
     } catch (error) {
-      console.error(`Error parsing JSON: ${jsonString}`);
+      console.log(`Err JSON PARSE: ${jsonString}`);
       return null;
     }
   });
 
   let messagesString = '';
   for (let i = 0; i < jsonArray.length; i++) {
-    if (jsonArray[i].isSuccess !== true || jsonArray[i].isToolMessage !== false) {
-      console.log(jsonArray[i]);
-      messagesString += "(Err💔r)";
-    } else {
-      messagesString += jsonArray[i].message;
+    if (jsonArray[i]) {
+      if (jsonArray[i].isSuccess !== true || jsonArray[i].isToolMessage !== false) {
+        console.log(jsonArray[i]);
+        messagesString += "(Err💔r)";
+      } else {
+        messagesString += jsonArray[i].message;
+      }
     }
   }
 
@@ -52,20 +54,18 @@ async function generate(messages, depth) {
   if (depth != 0){
     let bodydata = {
       "messages": messages,
+      "isAzure": false,
+      "azureApiVersion": "2023-08-01-preview",
       "stream": true,
       "model": "gpt-3.5-turbo-1106",
       "temperature": 0.8,
       "presence_penalty": 0.5,
       "frequency_penalty": 1,
       "top_p": 0.8,
-      "baseUrl": "/api/openai/",
-      "maxIterations": 1, //n ?
+      "baseUrl": "/api/openai",
+      "maxIterations": 10,
       "returnIntermediateSteps": true,
-      "useTools": [
-        "web-search",
-        "calculator",
-        "web-browser"
-      ]
+      "useTools": ["web-search", "calculator", "web-browser"]
     };
     let gpturl = "https://ngoctuanai-chatgptfree.hf.space/api/langchain/tool/agent"
     if (depth == 3){ // fault attempt
@@ -75,52 +75,48 @@ async function generate(messages, depth) {
       messages = messages.slice(0, 6) // less history maybe
       bodydata = {
         "messages": messages,
+        "isAzure": false,
+        "isAzure": false,
+        "azureApiVersion": "2023-08-01-preview",
         "stream": true,
         "model": "gpt-3.5-turbo-1106",
         "temperature": 0.8,
         "presence_penalty": 0.5,
-        "frequency_penalty": 0.9,
+        "frequency_penalty": 1,
         "top_p": 0.8,
-        "baseUrl": "/api/openai/",
-        "maxIterations": 10, //n ?
+        "baseUrl": "/api/openai",
+        "maxIterations": 10,
         "returnIntermediateSteps": true,
-        "useTools": [
-          "web-search",
-          "calculator",
-          "web-browser"
-        ]
+        "useTools": ["web-search", "calculator", "web-browser"]
       };
     }
 
     bodydata = JSON.stringify(bodydata, null, 2)
-    const response = await fetchWithTimeout(gpturl, 10000, {
+    const response = await fetch("https://ngoctuanai-chatgptfree.hf.space/api/langchain/tool/agent/edge", {
       "headers": {
-        "accept": "text/event-stream",
+        "accept": "application/json, text/event-stream",
         "accept-language": "en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
         "content-type": "application/json",
-        "sec-ch-ua": "\"Google Chrome\";v=\"119\", \"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"",
+        "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": "\"Windows\"",
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
-        "x-requested-with": "XMLHttpRequest"
+        "x-requested-with": "XMLHttpRequest",
+        "Referer": "https://ngoctuanai-chatgptfree.hf.space/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
       },
-      "referrer": "https://ngoctuanai-chatgptfree.hf.space/",
-      "referrerPolicy": "strict-origin-when-cross-origin",
       "body": bodydata,
-      "method": "POST",
-      "mode": "cors",
-      "credentials": "omit"
+      "method": "POST"
     });
-
     if (response && response.body) {
-      let val = await readStreamBody(response);
-      if (val == "Unauthorized - Access token is missing" || val == "Your authentication token has expired. Please try signing in again." || val == "(Err💔r)"){
+      let nresp = await readStreamBody(response)
+      if (nresp == "Unauthorized - Access token is missing" || nresp == "Your authentication token has expired. Please try signing in again." || nresp == "(Err💔r)"){
         console.log("Unauth FAIL DEPTH: ", depth)
-        val = generate(messages, depth-1)
+        nresp = generate(messages, depth-1)
       }
-      return val;
+      return nresp;
     } else {
       console.log("Timeout FAIL DEPTH: ", depth, response)
       let val = generate(messages, depth-1)
