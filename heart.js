91616598b62d09babfb5bdcd3ebcfc645428a28d
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-01-23 00:41:44 -0600

    Update heart.js

--- heart.js
+++ heart.js
@@ -1,59 +1,74 @@
-const hspacecookie = process.env['HSPACECOOKIE']
 const hspaceurl = process.env['HSPACEURL']
 const fetch = require('node-fetch');
 
 let heartbeats = 0
-async function uptimespace() {
-  try {
-    const response = await fetch(hspaceurl + "/join?", {
-      "headers": {
-        "accept": "*/*",
-        "accept-language": "en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-        "content-type": "application/json",
-        "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "cookie": hspacecookie,
-        "Referrer-Policy": "strict-origin-when-cross-origin"
-      },
-      "body": "{\"data\":[\"d\"],\"event_data\":null,\"fn_index\":0,\"trigger_id\":9,\"session_hash\":\"35ie5cow5sz\"}",
-      "method": "POST"
+const ws = require('ws');
+
+const HF_TOKEN = process.env['HF_TOKEN'];
+const hf_headers = {
+  "Content-Type": "application/json",
+  "Authorization": `Bearer ${HF_TOKEN}`
+};
+
+// by Staticaliza
+const gradio_generate = async (data, fn_index, space_url) => {
+  // Function to generate a simple hash
+  const generateHash = () => {
+    const timestamp = new Date().getTime();
+    const random = Math.random().toString(36).substring(2, 8);
+    return `${timestamp}_${random}`;
+  };
+
+  let session_hash = generateHash();
+  let json_data = JSON.stringify({ data: data, fn_index: fn_index, session_hash: session_hash });
+  let json_hash_data = JSON.stringify({ fn_index: fn_index, session_hash: session_hash });
+
+  return new Promise((mainResolve, mainReject) => {
+    const websocket = new ws(`wss://${space_url}queue/join`, { headers: hf_headers, maxPayload: 1024 * 1024 * 1024 });
+
+    websocket.on("open", () => {
+      //console.log("WS OPEN")
+      websocket.on("message", (event) => {
+        let resp = JSON.parse(event);
+        //console.log("WS EVENT " + resp.msg)
+        if (resp.msg === "queue_full") {
+          mainReject("[WEBSOCKET] Queue is full, please try again!");
+        }
+        else if (resp.msg === "send_hash") {
+          //console.log("sent hash data")
+          websocket.send(json_hash_data);
+        }
+        else if (resp.msg === "send_data") {
+          //console.log("sent data")
+          websocket.send(json_data);
+        }
+        else if (resp.msg === "process_completed") {
+          heartbeats += 1
+          console.log("[HEART] beat " + heartbeats)
+          mainResolve(resp.output);
+        };
+      });
     });
-    const response2 = await fetch(hspaceurl + "/data?session_hash=35ie5cow5sz", {
-      "headers": {
-        "accept": "text/event-stream",
-        "accept-language": "en-CA,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "no-cache",
-        "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "cookie": hspacecookie,
-        "Referrer-Policy": "strict-origin-when-cross-origin"
-      },
-      "body": null,
-      "method": "GET"
-    });
-    if (response2.status == "200"){ // success
-      heartbeats += 1
-      console.log("[heart] beat " + heartbeats)
-    } else {
-      throw "Ping Fialed"
-    }
-  } catch (err) {
-    console.log("[HEART ERROR]: " + err)
-  }
+
+    websocket.on("error", (error) => { mainReject(error); });
+  });
 }
 
+async function uptimespace(){
+  try {
+    const result = await gradio_generate(
+      ["run"],
+      1, 
+      hspaceurl
+    )
+  } catch (err) {
+    console.log("[HEART] ERR " + err)
+  }
+}
 function systole() {
   uptimespace()
   const pump = setInterval(uptimespace, 900000);
-  console.log("[HEART STARTED] " + pump)
+  console.log("[HEART STARTED] id " + pump)
 }
 
 module.exports = systole;
