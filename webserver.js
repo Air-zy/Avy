9043c4252cb9ea738171259388f536d942752194
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-03-02 20:22:32 -0600

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,6 +1,23 @@
 const express = require("express");
-const axios = require("axios");
 const server = express();
+
+const vowels = 'aeiou';
+const consonants = 'bcdfghjklmnpqrstvwxyz';
+
+function generateName(ip) {
+    const ipSegments = ip.split('.').join('').split(':').join('');
+    let name = '';
+    for (let i = 0; i < 6; i++) {
+        const index = (parseInt(ipSegments.charAt(i), 10) || 0) % 2 === 0 ? i % 2 : (i + 1) % 2;
+        name += index === 0 ? consonants.charAt(parseInt(ipSegments.charAt(i), 10) % consonants.length) : vowels.charAt(parseInt(ipSegments.charAt(i), 10) % vowels.length);
+    }
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    return name;
+}
+
+function wrapName(name) {
+    return `<span style="font-weight: bold; color: #b30000;">${name}</span>`;
+}
 
 let alvtimer = 0;
 let bigstring = ``;
@@ -8,7 +25,7 @@ server.all(`/`, (req, res) => {
   alvtimer += 1;
 
   const ipAddress = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.connection.remoteAddress;
-  bigstring += `<br>[${alvtimer}] ` + req.headers["user-agent"] + `<br>` + ipAddress + `<br>`;
+  bigstring += `<br>${wrapName(generateName(ipAddress))} [${alvtimer}]<br><i>` + req.headers["user-agent"] + `</i><br>` + ipAddress + `<br>`;
   
   res.send(`Air [SERVER]\n running ` + bigstring);
 });
