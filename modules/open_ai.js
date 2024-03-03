d8f29711348e519bb28b21516ffb132ac0eba2b9
Air-zy <120424474+Air-zy@users.noreply.github.com>
2024-03-02 20:25:31 -0600

    Update open_ai.js

--- modules/open_ai.js
+++ modules/open_ai.js
@@ -2,16 +2,53 @@ const fetch = require('node-fetch');
 const apiKey = process.env['aikey'];
 const fs = require('fs');
 
-let tokenusage = 0
+let firedb;
+
+let total_tokens = 0;
+let prompt_tokens = 0;
+let completion_tokens = 0;
+
+let tokenGetSuccess = 0;
+
+async function firedbTokenUpdate(tuTotal,tuInput,tuOutput) {
+  try {
+    if (tokenGetSuccess == 1) {
+      const aTuringRef = await firedb.doc('3rhrsWF9YlhpfZ2y3L7q');
+      aTuringRef.set(
+        {
+          'total_tokens': tuTotal,
+          'prompt_tokens': tuInput,
+          'completion_tokens': tuOutput
+        }
+      );
+    }
+  } catch(err) {
+    console.log("[FIRE BASE ERR]",err)
+  }
+}
+
+async function firedbTokenGET() {
+  try {
+    const aTuringRef = await firedb.doc('3rhrsWF9YlhpfZ2y3L7q');
+    const value = await aTuringRef.get();
+    const data = await value.data();
+    tokenGetSuccess = 1;
+    return data;
+  } catch(err) {
+    console.log("[FIRE BASE ERR]",err)
+  }
+}
+
 async function generate(msgs, aimodel) {
   const body = {
     "messages": msgs,
     "model": aimodel,
-    "temperature": 0.8,
-    "presence_penalty": 0.5,
-    "frequency_penalty": 1,
+    "temperature": 1,
+    "presence_penalty": 0.9,
+    "frequency_penalty": 1.5,
     "top_p": 0.8,
     "logit_bias": {
+        "5781": 1, // avy
         "68697": -100, // sorry
         "19701": -100, // Sorry
         "3923": -50, // What
@@ -22,16 +59,24 @@ async function generate(msgs, aimodel) {
         "58369": -50, // digital
         "26752": -50, // virtual
         "82630": -50, // friendly
+        "47916": -50, // Whatever
+        "69386": -50, // whatever
+        "1480": 5, // Error
         "31": -100, // @
-        "93": 5, // ~
-        "2201": 10, // no
+        "93": 1, // ~
+        "2727": 5, //me
+        "2201": 5, // no
         "596": -10, // 's
         "19643": 5, // sure
         "9336": 5, // well
         "2079": -50, // request
         "29589": -50, // :)
-        "9": 5, // *
-        "0": -30 // !
+        "9": 1, // *
+        "0": -1, // !
+        "30": -1, // ?
+        "43012": -1, // (playing)
+        "3235": -1, // ( along)
+        "19770": 1 // 418
     }
   }
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -44,11 +89,16 @@ async function generate(msgs, aimodel) {
   });
   const nresp = await response.json()
   if ('usage' in nresp) {
-    const tokensUsed = nresp.usage.total_tokens;
-    tokenusage += tokensUsed
+    const tokensUsedTotal = nresp.usage.total_tokens;
+    const tokensUsedInput = nresp.usage.prompt_tokens;
+    const tokensUsedOutput = nresp.usage.completion_tokens;
+    total_tokens += tokensUsedTotal
+    prompt_tokens += tokensUsedInput;
+    completion_tokens += tokensUsedOutput;
     
+    firedbTokenUpdate(total_tokens, prompt_tokens, completion_tokens);
     let update = JSON.parse(fs.readFileSync('json_storage/info.json', 'utf-8'));
-    update.token_usage = tokenusage;
+    update.token_usage = total_tokens;
     fs.writeFileSync('json_storage/info.json', JSON.stringify(update, null, 2));
   }
   if (nresp["choices"]) {
@@ -58,11 +108,19 @@ async function generate(msgs, aimodel) {
   }
 }
 
-function getusage(){
-  return tokenusage
+async function loadTokens() {
+  const newtokendata = await firedbTokenGET()
+  total_tokens = parseInt(newtokendata.total_tokens);
+  prompt_tokens = parseInt(newtokendata.prompt_tokens);
+  completion_tokens = parseInt(newtokendata.completion_tokens);
+}
+
+function pass_exports(p_firedb) {
+  firedb = p_firedb;
+  loadTokens();
 }
 
 module.exports = {
   generate,
-  getusage
+  pass_exports
 };
